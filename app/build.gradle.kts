efc320eb28a424b5e2761c32bacaf5295fef3650
suleman81 <sunnybajwa904@gmail.com>
2023-12-05 16:18:55 +0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.moutamid.secretservice"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         setProperty("archivesBaseName", "SecretService-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
